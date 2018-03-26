657f813fdec29ecff77ad17a4432454ec6c176e3
TARO JINKAWA <jtaroxxx@gmail.com>
2018-03-26 17:25:16 +0900

    PTra18_04まで完了

--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -20,8 +28,65 @@ public class PTra18_04 {
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
 
+		ArrayList<Player> array = new ArrayList<Player>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+				Player player = new Player();
+				String line = scanner.nextLine();
+				String[] list = line.split(",", 4);
+
+				player.setPosition(list[0]);
+				player.setName(list[1]);
+				player.setCountry(list[2]);
+				player.setTeam(list[3]);
+
+				array.add(player);
+
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+		Collections.shuffle(array);
 
+		int gkCount = 0;
+		int dfCount = 0;
+		int mfCount = 0;
+		int fwCount = 0;
+
+		for (Player playerd : array) {
+			
+			
+			if(playerd.getPosition().equals("GK") ){
+				if (gkCount < 1) {
+					System.out.println(playerd.toString());
+					gkCount++;
+				}
+
+			}else if(playerd.getPosition().equals("DF") ){
+				if (dfCount < 4) {
+					System.out.println(playerd.toString());
+					dfCount++;
+				}
+
+			}else if(playerd.getPosition().equals("MF") ){
+				if (mfCount < 4) {
+					System.out.println(playerd.toString());
+					mfCount++;
+				}
+
+			}else if(playerd.getPosition().equals("FW") ){
+				if (fwCount < 2) {
+					System.out.println(playerd.toString());
+					fwCount++;
+				}
+
+			}
+
+
+		}
 	}
+
 }
